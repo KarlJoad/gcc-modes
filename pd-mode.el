fd20bcf85e70b75e157f954ce3e424cceea48d15
Raven Hallsby <raven@hallsby.com>
2025-12-09 23:05:31 -0600

    Initial commit

---
+++ pd-mode.el
@@ -0,0 +1,36 @@
+;;; pd-mode.el --- Major mode for GCC PD files -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Raven Hallsby
+
+;; Author: Raven Hallsby <raven@hallsby.com>
+;; Maintainer: Raven Hallsby <raven@hallsby.com>
+;; Created: Decmeber 9, 2025
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "29.1"))
+;; Keywords: PD, tree-sitter
+;; URL: https://github.com/KarlJoad/gcc-modes
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See the README.org file for details.
+
+
+;;; Code:
+
+(provide 'pd-mode)
+;;; pd-mode.el ends here
